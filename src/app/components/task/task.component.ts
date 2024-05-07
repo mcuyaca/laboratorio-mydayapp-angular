0f3ca0c943b82cf1f796b65b3868b20a59a748d4
mcuya.ca <mcuya.ca@gmail.com>
2024-05-06 21:46:23 -0500

    add task, taskList and footer components

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Task } from '@models/task.model';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css'],
+})
+export class TaskComponent {
+  @Input() task: Task = {
+    id: '',
+    title: '',
+    completed: false,
+  };
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '@models/task.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  constructor() {}
 
-  constructor() { }
+  data: Task[] = [];
 
-  ngOnInit(): void {
+  newTitle: string = '';
+  addTask() {
+    if (this.newTitle.trim() === '') return;
+    const newTask: Task = {
+      id: this.data.length.toString(),
+      title: this.newTitle,
+      completed: false,
+    };
+    this.data = [...this.data, newTask];
+    this.newTitle = '';
   }
 
+  deleteTask(id: string) {
+    this.data.filter((value, index) => index !== Number(id));
+  }
 }
